bf8f8ec9592e2489bd6254d347064b9931d003d5
arthur <wr80340@smail.nchu.edu.tw>
2025-03-23 00:47:17 +0800

    #partially modify to postgresql

--- sql_to_vector_db_with_langgraph.py
+++ sql_to_vector_db_with_langgraph.py
@@ -1,8 +1,8 @@
 # %%
 # https://python.langchain.com/docs/tutorials/sql_qa/
 import json
-from typing import Annotated, Dict, List, Literal, TypedDict
 
+import psycopg2
 from IPython.display import Image, display
 from langchain_community.utilities import SQLDatabase
 from langchain_core.documents import Document
@@ -15,28 +15,69 @@ from langgraph.types import Send
 from pydantic import BaseModel, Field
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, PointStruct, VectorParams
+from typing_extensions import Annotated, Dict, List, Literal, TypedDict
 
 from langchain.cache import InMemoryCache
 from langchain.globals import set_debug, set_llm_cache
+from langchain.tools import BaseTool, StructuredTool, tool
 
 set_llm_cache(InMemoryCache())
 set_debug(False)
 
-with open("api_keys.json") as f:
-    api_keys = json.loads(f.read())
+with open("config.json") as f:
+    config = json.loads(f.read())
 model = "deepseek-r1:14b"
 collection = "Chinook_DB_table_summary"
+# %%
 # setup DB
-db = SQLDatabase.from_uri(database_uri="sqlite:///Chinook.db")
-db._sample_rows_in_table_info = 3
+db = psycopg2.connect(**config.get("postgres"))
+# %%
+
+
+@tool
+def get_pg_table_list(database: psycopg2.extensions.connection) -> list[str]:
+    """Get a list of tables in the database."""
+    with database.cursor() as curs:
+        curs.execute(
+            """
+            SELECT table_name 
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            """
+        )
+        return [row[0] for row in curs.fetchall()]
+
+
+@tool
+def get_pg_related_tables(
+    database: psycopg2.extensions.connection, table_name: str
+) -> list[str]:
+    """Find all the table related with table input."""
+    with database.cursor() as curs:
+        curs.execute(
+            f"""
+            SELECT 
+                ccu.table_name AS referenced_table
+            FROM information_schema.table_constraints AS tc
+            JOIN information_schema.key_column_usage AS kcu
+                ON tc.constraint_name = kcu.constraint_name
+            JOIN information_schema.constraint_column_usage AS ccu
+                ON ccu.constraint_name = tc.constraint_name
+            WHERE 1=1
+                AND tc.constraint_type = 'FOREIGN KEY' 
+                AND tc.table_name = '{table_name}';
+            """
+        )
+        return [row[0] for row in curs.fetchall() if row[0] != table_name]
 
 
 # %% stage 1, inspect each table
 class OverallSQLState(TypedDict):
-    db: SQLDatabase
+    db: psycopg2.extensions.connection
     owner: str
     dialect: str
     tables: Dict[str, str | None]
+    schemas: Dict[str, str | None]
 
 
 class SingleSQLDistillState(TypedDict):
@@ -50,7 +91,7 @@ sql_schema_prompt = ChatPromptTemplate.from_messages(
         (
             "user",
             "Here is the SQLite database schema information. "
-            "Please analyze its table structures and provide a detailed explanation. {schema}.",
+            "Please analyze table structures and provide a detailed explanation. {schema}.",
         ),
         (
             "assistant",
@@ -63,17 +104,23 @@ model = ChatOllama(model=model, temperature=0)
 
 
 # %%
+@tool
 def sql_info_distill(data_sample: str) -> str:
+    """Distill SQL table information."""
     return model.invoke(
         input=sql_schema_prompt.invoke(input=data_sample),
     ).content
 
 
 def get_basic_infos(state: OverallSQLState):
-    return {"dialect": state["db"].dialect}
+    """Get basic information about the database."""
+    return {
+        "dialect": state["db"].dialect,
+    }
 
 
 def get_table_infos(state: OverallSQLState):
+    """Get information about each table in the database."""
     return {
         "tables": {
             table: {
@@ -85,16 +132,16 @@ def get_table_infos(state: OverallSQLState):
                         .fetchall()
                     ]
                 ),
-                "desc": sql_info_distill(
-                    data_sample=(
-                        db.run(
-                            command=f"SELECT * FROM {table} LIMIT 10;",
-                            include_columns=True,
-                        )
-                        .replace("\n", "")
-                        .replace("\t", "")
-                        .replace("\t", "")
+                "schema": db.get_table_info([table])
+                .replace("\n", "")
+                .replace("\t", ""),
+                "sample_data": (
+                    db.run(
+                        command=f"SELECT * FROM {table} LIMIT 10;",
+                        include_columns=True,
                     )
+                    .replace("\n", "")
+                    .replace("\t", "")
                 ),
             }
             for table in state["db"].get_usable_table_names()
@@ -112,12 +159,12 @@ graph.add_edge(START, "get_table_infos")
 app = graph.compile()
 # %%
 
-display(Image(app.get_graph().draw_mermaid_png()))
+# display(Image(app.get_graph().draw_mermaid_png()))
 # %%
 for s in app.stream(
     {
         "owner": "Arthur",
-        "db": SQLDatabase.from_uri(database_uri="sqlite:///Chinook.db"),
+        "db": SQLDatabase.from_uri(database_uri=database_uri),
     }
 ):
     print(s)
