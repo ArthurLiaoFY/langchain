73a6c35fde74db8d2c3275fa6b6a44385a43b92d
arthur <wr80340@smail.nchu.edu.tw>
2025-03-24 12:00:39 +0800

    #add node infos

--- agent_framework/core/nodes/pg_nodes.py
+++ agent_framework/core/nodes/pg_nodes.py
@@ -5,11 +5,12 @@ from agent_framework.core.tools.pg_utils import (
     close_connection,
     connect_db,
     connection,
-    get_related_tables,
+    get_related_tables_desc,
     get_relationship_desc,
     get_sample_data,
     get_table_columns,
     get_table_list,
+    get_table_primary_key,
     query,
 )
 
@@ -20,12 +21,36 @@ def connect_db_node(state: PostgresDatabaseState) -> Dict[connection | None, boo
     )
 
     return {
+        "dialect": "Postgres",
         "database": conn,
         "recursion_time": 0 if conn is not None else state["recursion_time"] + 1,
         "is_connected": True if conn is not None else False,
     }
 
 
-def get_table_infos(state: PostgresDatabaseState):
+def get_database_common_infos_node(state: PostgresDatabaseState):
 
-    pass
+    return {
+        "tables": {
+            table_name: {
+                "columns": get_table_columns.invoke(
+                    input={"database": state["database"], "table_name": table_name}
+                ),
+                "primary_key": get_table_primary_key.invoke(
+                    input={"database": state["database"], "table_name": table_name}
+                ),
+                # "sample_data": get_sample_data.invoke(
+                #     input={"database": state["database"], "table_name": table_name}
+                # ),
+                "related_tables_desc": get_related_tables_desc.invoke(
+                    input={"database": state["database"], "table_name": table_name}
+                ),
+                "relationship_desc": get_relationship_desc.invoke(
+                    input={"database": state["database"], "table_name": table_name}
+                ),
+            }
+            for table_name, in zip(
+                get_table_list.invoke(input={"database": state["database"]})
+            )
+        },
+    }
