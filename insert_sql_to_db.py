e5f16ec49258d99f62df38fbf53f0b4c43425425
arthur <wr80340@smail.nchu.edu.tw>
2025-03-24 17:00:48 +0800

    #tmp save

--- insert_sql_to_db.py
+++ insert_sql_to_db.py
@@ -6,17 +6,18 @@ from IPython.display import Image, display
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langgraph.errors import GraphRecursionError
 from langgraph.graph import END, START, StateGraph
+from langgraph.types import Send
 from psycopg2.extensions import connection
 from typing_extensions import Annotated, Dict, List, TypedDict
 
 from agent_framework.core.nodes.pg_nodes import (
     connect_database_node,
+    extract_table_info_node,
     get_database_common_info_node,
-    question_answering_node,
+    upsert_to_vector_database_node,
 )
 from agent_framework.core.routes.pg_routes import reconnect_db
 from agent_framework.core.states.pg_states import PostgresDatabaseState, TableState
-from agent_framework.core.tools.model_utils import load_llm_model
 from agent_framework.core.tools.qdrant_utils import (
     connect_collection_vector_store,
     connect_qdrant_client,
@@ -39,29 +40,30 @@ with open("config.json") as f:
 # %%
 # load model to state
 
+# %%
+
 
 # %%
 graph = StateGraph(PostgresDatabaseState)
 graph.add_node("connect_db", connect_database_node)
 graph.add_node("get_database_common_info", get_database_common_info_node)
-graph.add_node("question_answering", question_answering_node)
+graph.add_node("extract_table_info", extract_table_info_node)
+graph.add_node("upsert_to_vector_database", upsert_to_vector_database_node)
 # graph.add_node("get_table_list", get_table_list)
 # graph.add_node("run_table_info", RunnableParallel(context=RunnablePassthrough()))
 
 
 graph.add_edge(START, "connect_db")
 graph.add_conditional_edges("connect_db", reconnect_db)
-graph.add_edge("get_database_common_info", "question_answering")
-graph.add_edge("question_answering", END)
+graph.add_edge("get_database_common_info", "extract_table_info")
+graph.add_edge("extract_table_info", "upsert_to_vector_database")
+graph.add_edge("upsert_to_vector_database", END)
 
 
 app = graph.compile()
 # %%
 for s in app.stream(
     {
-        "llm_model": load_llm_model(
-            model_name=config.get("model", {}).get("deepseek", {}).get("model_name")
-        ),
         "postgres_connection_info": secrets.get("postgres"),
         "recursion_time": 0,
         "recursion_limit": config.get("database", {}).get("recursion_limit"),
