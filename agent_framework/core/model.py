9e13f6a4e1f629688e655131d34625aac52c8915
arthur <wr80340@smail.nchu.edu.tw>
2025-03-24 16:30:02 +0800

    #modify config

--- agent_framework/core/model.py
+++ agent_framework/core/model.py
@@ -5,4 +5,7 @@ from langchain_ollama import ChatOllama
 with open("config.json") as f:
     config = json.loads(f.read())
 
-llm_model = ChatOllama(model=config.get("model_name", "deepseek-r1:14b"), temperature=0)
+llm_model = ChatOllama(
+    model=config.get("llm_model", {}).get("model_name", "deepseek-r1:14b"),
+    temperature=0,
+)
