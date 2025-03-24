a57149cb06f99e2ee47d38a231b09a29fab45e82
arthur <wr80340@smail.nchu.edu.tw>
2025-03-24 19:56:23 +0800

    #remove secrets

--- agent_test.py
+++ agent_test.py
@@ -1,15 +1,18 @@
 # %%
+import json
+
+with open("secrets.json") as f:
+    secrets = json.loads(f.read())
+
+with open("config.json") as f:
+    config = json.loads(f.read())
+
+# %%
 from agent_framework.core.agent.connect_pg import connect_postgres_agent
 
 connect_postgres_agent().invoke(
     {
-        "postgres_connection_info": {
-            "host": "localhost",
-            "port": 5432,
-            "dbname": "chinook",
-            "user": "wr80340",
-            "password": "1qaz2wsx3edc4rfv",
-        },
+        "postgres_connection_info": secrets.get("postgres"),
         "recursion_time": 0,
         "recursion_limit": 4,
     }
@@ -23,13 +26,7 @@ extract_table_summary_agent().invoke(
     {
         "database": connect_postgres_agent().invoke(
             {
-                "postgres_connection_info": {
-                    "host": "localhost",
-                    "port": 5432,
-                    "dbname": "chinook",
-                    "user": "wr80340",
-                    "password": "1qaz2wsx3edc4rfv",
-                },
+                "postgres_connection_info": secrets.get("postgres"),
                 "recursion_time": 0,
                 "recursion_limit": 4,
             }
