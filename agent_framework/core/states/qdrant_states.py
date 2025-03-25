a512cbdedd7233b2d2c9e5ad0df9ee65f9d51686
arthur <wr80340@smail.nchu.edu.tw>
2025-03-25 14:01:54 +0800

    #init qdrant stare

---
+++ agent_framework/core/states/qdrant_states.py
@@ -0,0 +1,29 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from typing_extensions import Dict, TypedDict, Union
+
+
+class QdrantConnectionInfo(TypedDict):
+    # ---------------------------
+    qdrant_connection_info: Dict[str, str]
+    recursion_limit: int
+    # ---------------------------
+    qdrant_client: Union[QdrantClient, None]
+    recursion_time: int
+    is_connected: bool
+    # ---------------------------
+    collection: str
+
+
+class QdrantClientState(TypedDict):
+    # ---------------------------
+    qdrant_client: QdrantClient
+    collection: str
+    # ---------------------------
+    llm_vector_size: int
+    llm_embd: OllamaEmbeddings
+    # ---------------------------
+    vector_store: Union[QdrantVectorStore, None]
+    # ---------------------------
+    debug: bool
