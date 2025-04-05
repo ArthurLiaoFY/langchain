1c6b8a4aeba602a1b6b1fdcb8697ac6e248c02dc
arthur <wr80340@smail.nchu.edu.tw>
2025-04-05 12:18:43 +0800

    #restart

--- agent_framework/core/states/init_states.py
+++
@@ -1,8 +0,0 @@
-from typing_extensions import Annotated, Dict, List, TypedDict, Union
-
-from agent_framework.core.states.pg_states import DatabaseState
-
-
-class InitState(TypedDict):
-    # a chat bot
-    database_related: DatabaseState
