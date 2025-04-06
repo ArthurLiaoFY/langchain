d1a1b26c5bc2db8e3b29b996364fda6701551259
arthur <wr80340@smail.nchu.edu.tw>
2025-04-06 16:07:38 +0800

    #good ver

--- state_with_react_agnet.py
+++ state_with_react_agnet.py
@@ -1,38 +1,129 @@
 # %%
-from typing import List
 
+from typing import Literal
+
+from langchain_anthropic import ChatAnthropic
+from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
-from langgraph.graph import END, START, MessagesState, StateGraph
-from langgraph.prebuilt import create_react_agent
-from langgraph.prebuilt.chat_agent_executor import AgentState
+from langgraph.graph import END, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, create_react_agent
+from pydantic import BaseModel, Field
 
-
-class BaseState(AgentState):
-    schema: List[int]
-
+from langchain.tools import tool
 
 model = ChatOllama(
     model="qwen2.5:14b",
     temperature=0,
 )
-graph = StateGraph(BaseState)
-# graph = StateGraph(AgentState)
 
 
-agent = create_react_agent(
-    model=model,
-    tools=[],
-    state_schema=BaseState,
+class WeatherResponse(BaseModel):
+    """Respond to the user with this"""
+
+    temperature: float = Field(description="The temperature in fahrenheit")
+    wind_directon: str = Field(
+        description="The direction of the wind in abbreviated form"
+    )
+    wind_speed: float = Field(description="The speed of the wind in km/h")
+
+
+# Inherit 'messages' key from MessagesState, which is a list of chat messages
+class AgentState(MessagesState):
+    # Final structured response from the agent
+    final_response: WeatherResponse
+
+
+@tool
+def get_weather(city: Literal["nyc", "sf"]):
+    """Use this to get weather information."""
+    if city == "nyc":
+        return "It is cloudy in NYC, with 5 mph winds in the North-East direction and a temperature of 70 degrees"
+    elif city == "sf":
+        return "It is 75 degrees and sunny in SF, with 3 mph winds in the South-East direction"
+    else:
+        raise AssertionError("Unknown city")
+
+
+tools = [get_weather]
+
+
+model_with_tools = model.bind_tools(tools)
+model_with_structured_output = model.with_structured_output(WeatherResponse)
+
+tools = [get_weather, WeatherResponse]
+
+# Force the model to use tools by passing tool_choice="any"
+model_with_response_tool = model.bind_tools(tools, tool_choice="any")
+
+
+# Define the function that calls the model
+def call_model(state: AgentState):
+    response = model_with_response_tool.invoke(state["messages"])
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+# Define the function that responds to the user
+def respond(state: AgentState):
+    # Construct the final answer from the arguments of the last tool call
+    weather_tool_call = state["messages"][-1].tool_calls[0]
+    response = WeatherResponse(**weather_tool_call["args"])
+    # Since we're using tool calling to return structured output,
+    # we need to add  a tool message corresponding to the WeatherResponse tool call,
+    # This is due to LLM providers' requirement that AI messages with tool calls
+    # need to be followed by a tool message for each tool call
+    tool_message = {
+        "type": "tool",
+        "content": "Here is your structured response",
+        "tool_call_id": weather_tool_call["id"],
+    }
+    # We return the final answer
+    return {"final_response": response, "messages": [tool_message]}
+
+
+# Define the function that determines whether to continue or not
+def should_continue(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is only one tool call and it is the response tool call we respond to the user
+    if (
+        len(last_message.tool_calls) == 1
+        and last_message.tool_calls[0]["name"] == "WeatherResponse"
+    ):
+        return "respond"
+    # Otherwise we will use the tool node again
+    else:
+        return "continue"
+
+
+# Define a new graph
+workflow = StateGraph(AgentState)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("respond", respond)
+workflow.add_node("tools", ToolNode(tools))
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.set_entry_point("agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    "agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "respond": "respond",
+    },
 )
 
-graph.add_node("agent", agent)
-graph.add_edge(START, "agent")
-graph.add_edge("agent", END)
-chain = graph.compile()
-
-a = chain.invoke({"messages": [("human", "Hi")], "schema": [1, 2, 3]})
-
-
+workflow.add_edge("tools", "agent")
+workflow.add_edge("respond", END)
+graph = workflow.compile()
+answer = graph.invoke(input={"messages": [("human", "what's the weather in SF?")]})[
+    "final_response"
+]
 # %%
-a
+answer
 # %%
